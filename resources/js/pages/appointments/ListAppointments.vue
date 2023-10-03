926b87bd638d6cfc8f25d365db6c9c8e8d090f4b
Sian <simple0504@hotmail.com>
2023-10-03 16:25:06 +0800

    Update the page.

--- resources/js/pages/appointments/ListAppointments.vue
+++ resources/js/pages/appointments/ListAppointments.vue
@@ -16,44 +16,196 @@
             </div>
         </div>
     </div>
-    <!-- <div class="content">
+    <div class="content">
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p><a class="card-link" href="#">Card link</a> <a class="card-link" href="#">Another link</a>
+            <div class="row phtoo-box">
+                <div class="col-12">
+                    <div class="card-columns">
+                        <div class="card">
+                            <input type="checkbox" id="checkthis2" />
+                            <label for="checkthis2"><i class="fas fa-check"></i></label>
+                            <a class="glightbox" href="https://images.pexels.com/photos/2403569/pexels-photo-2403569.jpeg" data-toggle="lightbox" data-title="sample 1 - white" data-gallery="gallery">
+                                <img src="https://images.pexels.com/photos/2403569/pexels-photo-2403569.jpeg" class="img-fluid" alt="black sample"/>
+                            </a>
                         </div>
-                    </div>
-                    <div class="card card-primary card-outline">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p><a class="card-link" href="#">Card link</a> <a class="card-link" href="#">Another link</a>
+                        <div class="card">
+                            <input type="checkbox" id="checkthis3" />
+                            <label for="checkthis3"><i class="fas fa-check"></i></label>
+                            <a class="glightbox" href="https://images.pexels.com/photos/3650469/pexels-photo-3650469.jpeg" data-toggle="lightbox" data-title="sample 1 - white" data-gallery="gallery">
+                                <img src="https://images.pexels.com/photos/3650469/pexels-photo-3650469.jpeg" class="img-fluid" alt="red sample"/>
+                            </a>
                         </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="card">
-                        <div class="card-header">
-                            <h5 class="m-0">Featured</h5>
+                        <div class="card">
+                            <input type="checkbox" id="checkthis4" />
+                            <label for="checkthis4"><i class="fas fa-check"></i></label>
+                            <a class="glightbox" href="https://images.pexels.com/photos/1128782/pexels-photo-1128782.jpeg" data-toggle="lightbox" data-title="sample 1 - white" data-gallery="gallery">
+                                <img src="https://images.pexels.com/photos/1128782/pexels-photo-1128782.jpeg" class="img-fluid" alt="red sample"/>
+                            </a>
                         </div>
-                        <div class="card-body">
-                            <h6 class="card-title">Special title treatment</h6>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p><a class="btn btn-primary" href="#">Go somewhere</a>
+                        <div class="card">
+                            <input type="checkbox" id="checkthis5" />
+                            <label for="checkthis5"><i class="fas fa-check"></i></label>
+                            <a class="glightbox" href="https://images.pexels.com/photos/3342697/pexels-photo-3342697.jpeg" data-toggle="lightbox" data-title="sample 1 - white" data-gallery="gallery">
+                                <img src="https://images.pexels.com/photos/3342697/pexels-photo-3342697.jpeg" class="img-fluid" alt="white sample"/>
+                            </a>
                         </div>
-                    </div>
-                    <div class="card card-primary card-outline">
-                        <div class="card-header">
-                            <h5 class="m-0">Featured</h5>
+                        <div class="card">
+                            <input type="checkbox" id="checkthis6" />
+                            <label for="checkthis6"><i class="fas fa-check"></i></label>
+                            <a class="glightbox" href="https://images.pexels.com/photos/3585798/pexels-photo-3585798.jpeg" data-toggle="lightbox" data-title="sample 1 - white" data-gallery="gallery">
+                                <img src="https://images.pexels.com/photos/3585798/pexels-photo-3585798.jpeg" class="img-fluid" alt="black sample"/>
+                            </a>
                         </div>
-                        <div class="card-body">
-                            <h6 class="card-title">Special title treatment</h6>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p><a class="btn btn-primary" href="#">Go somewhere</a>
+                        <div class="card">
+                            <input type="checkbox" id="checkthis7" />
+                            <label for="checkthis7"><i class="fas fa-check"></i></label>
+                            <a class="glightbox" href="https://images.pexels.com/photos/1082024/pexels-photo-1082024.jpeg" data-toggle="lightbox" data-title="sample 1 - white" data-gallery="gallery">
+                                <img src="https://images.pexels.com/photos/1082024/pexels-photo-1082024.jpeg" class="img-fluid" alt="black sample"/>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div> -->
+    </div>
 </template>
+
+
+<style scope>
+    .btn-box a{
+        font-size: 0.875rem;
+        font-weight: 300;
+        cursor: pointer;
+        outline: 0;
+        user-select: none;
+        border-color: rgb(216, 216, 216) rgb(209, 209, 209) rgb(186, 186, 186);
+        border-style: solid;
+        border-radius: 4px;
+        border-width: 1px;
+        background-color: hsl(0, 0%, 100%);
+        color: hsl(0, 0%, 29%);
+        padding: 8px;
+        text-decoration: none;
+        display: inline-block;
+    }
+
+    .btn-box a.btn-delete{
+        color: hsl(0deg 86.96% 46.75%);
+        border-color: hsl(0deg 82.16% 64.69%);
+    }
+
+    .btn-box .btn-delete:hover {
+        border-color: hsl(0deg 100% 49.61%);
+        background-color: hsl(0deg 23.26% 97.95%);
+    }
+
+    .btn-box .btn-delete:active {
+        background-color: hsl(0deg 76.84% 89.8%);
+    }
+
+    .file input[type='file'] {
+        display: none;
+    }
+
+    .file label,
+    .file button {
+        font-size: 0.875rem;
+        font-weight: 300;
+        cursor: pointer;
+        outline: 0;
+        user-select: none;
+        border-color: rgb(216, 216, 216) rgb(209, 209, 209) rgb(186, 186, 186);
+        border-style: solid;
+        border-radius: 4px;
+        border-width: 1px;
+        background-color: hsl(0, 0%, 100%);
+        color: hsl(0, 0%, 29%);
+        padding: 8px;
+    }
+
+    .file label:hover,
+    .file button:hover {
+        border-color: hsl(0, 0%, 21%);
+    }
+
+    .file label:active,
+    .file button:active {
+        background-color: hsl(0, 0%, 96%);
+    }
+
+    .file label > i,
+    .file button > i {
+        padding-right: 5px;
+    }
+
+    .file-upload > label,
+    .file-upload > button {
+        color: hsl(204, 86%, 53%);
+        border-color: hsl(204, 86%, 53%);
+    }
+
+    .file-upload > label:hover,
+    .file-upload > button:hover {
+        border-color: hsl(204, 86%, 53%);
+        background-color: hsl(204, 86%, 96%);
+    }
+
+    .file-upload > label:active,
+    .file-upload > button:active {
+        background-color: hsl(204, 86%, 91%);
+    }
+
+    .phtoo-box .card-columns a {
+        display: inline-block;
+    }
+
+    .phtoo-box .card-columns i.fas{
+        line-height: unset;
+    }
+
+    .phtoo-box .card-columns input[type="checkbox"][id^="checkthis"] {
+        display: none;
+    }
+
+    .phtoo-box .card-columns label {
+        display: block;
+        position: absolute;
+        top: 5px;
+        right: 5px;
+        width: 25px;
+        height: 25px;
+        cursor: pointer;
+        background-color: white;
+        color: white;
+        border: 1px solid #07b21e;
+        border-radius: 50%;
+        text-align: center;
+        line-height: 22px;
+        transition-duration: 0.4s;
+        transform: scale(0);
+        z-index: 999;
+    }
+
+    .phtoo-box .card-columns .card:hover label{
+        transform: scale(1);
+    }
+
+    .phtoo-box .card-columns  :checked + label {
+        color: #07b21e;
+        transform: scale(1);
+    }
+
+    @media (min-width: 768px) {  
+        .phtoo-box .card-columns {column-count: 4;}
+    }
+
+    @media (min-width: 992px) { 
+        .phtoo-box .card-columns {column-count: 4;}
+    }
+
+    @media (min-width: 1200px) {  
+        .phtoo-box .card-columns {column-count: 5;} 
+    }
+    
+
+</style>
--- resources/js/pages/setting/UpdateSetting.vue
+++ resources/js/pages/setting/UpdateSetting.vue
@@ -40,14 +40,23 @@
         <div class="modal fade" id="modal-default">
             <div class="modal-dialog">
                 <div class="modal-content">
-                    <div>
-                        <div v-bind="getRootProps()">
-                        <input v-bind="getInputProps()" />
-                        <p v-if="isDragActive">Drop the files here ...</p>
-                        <p v-else>Drag 'n' drop some files here, or click to select files</p>
-                        </div>
-                        <button @click="open">open</button>
-                    </div>
+    <div>
+        <div v-if="resfiles.length > 0" v-for="(file,i) in resfiles" :key="i">
+            <span class="block text-xs px-2 py-1 border relative">
+                {{ file.filename }}
+                <i @click="remove(file)" class="fa fa-xmark -right-2 -top-2 cursor-pointer absolute w-4 h-4 bg-primary text-white rounded-full !flex items-center justify-center"></i>
+            </span>
+        </div>
+        <div v-else v-bind="getRootProps()">
+            <input v-bind="getInputProps()" />
+            <div class="text-center border py-3 px-2 cursor-pointer">
+                <svg class="w-7 mx-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                    <path class="fill-primary" d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z"/>
+                </svg>
+               <span class="text-xs"> Drag & Drop your files</span>
+            </div>
+        </div>
+   </div>
                 </div>
             <!-- /.modal-content -->
             </div>
